e5491089b17df2594df22469d3a626e3e81cfab6
Brielle Woods <brielle@Brielles-MacBook-Air.local>
2017-05-02 15:48:25 -0400

    dating profile

--- Script.js
+++ Script.js
@@ -1,23 +1,28 @@
-var FirstNameInput = document.getElementById('FirstName')
-var LastNameInput = document.getElementById('LastName')
+var firstNameInput = document.getElementById('firstName')
+var lastNameInput = document.getElementById('lastName')
 var discriptionInput = document.getElementById('discription')
 var emailInput = document.getElementById('email')
 var phoneInput = document.getElementById('phone')
 var previewParagraph = document.getElementById('preview')
+
 var updatepreview = function () {
-  var FirstName = FirstNameInput.value
-  var LastName = LastNameInput.value
+  var firstName = firstNameInput.value
+  var lastName = lastNameInput.value
   var discription = discriptionInput.value
   var email = emailInput.value
   var phone = phoneInput.value
-  var AllInfo = "<h1> Hi, my name is <strong>" + FirstName +'</strong> </h1> <strong>'+ LastName +'!</strong> <strong>' + discription + "!</strong> If you're interested in a date, you can email me at <strong>" + email +'</strong> or give me a call at <strong>' + phone + '</strong>'
+  var AllInfo =
+    '<h1> Hi, my name is <strong>' + firstName + '</strong> </h1>' +
+    '<strong>' + lastName + '!</strong> <strong>' + discription + '!</strong>' +
+    'If you\'re interested in a date, you can email me at <strong>' + email +
+    '</strong> or give me a call at <strong>' + phone + '</strong>'
   previewParagraph.innerHTML = AllInfo
   var paragraph = document.getElementById('my-paragraph')
   paragraph.textContent = AllInfo
 }
 
-FirstNameInput.addEventListener('input', updatepreview)
-LastNameInput.addEventListener('input', updatepreview)
+firstNameInput.addEventListener('input', updatepreview)
+lastNameInput.addEventListener('input', updatepreview)
 discriptionInput.addEventListener('input', updatepreview)
 emailInput.addEventListener('input', updatepreview)
 phoneInput.addEventListener('input', updatepreview)
